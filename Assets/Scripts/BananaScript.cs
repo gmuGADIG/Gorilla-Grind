c3f38eac5cf8e3e6d1b88b2c5ef2cca66b2f2baf
Jeb Hunt <jebhunt384@yahoo.com>
2023-03-08 21:19:03 -0500

    I learned how to make moving sprites by slicing them and I worked on banana script.

--- Assets/Scripts/BananaScript.cs
+++ Assets/Scripts/BananaScript.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class BananaScript : MonoBehaviour
 {
+    public BoxCollider2D bananaCollider;
+    public CapsuleCollider2D playerCollider;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -19,5 +22,12 @@ public class BananaScript : MonoBehaviour
     public void collect()
     {
         GameObject.Destroy(gameObject);
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
     }
 }
