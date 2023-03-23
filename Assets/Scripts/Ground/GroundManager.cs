431e85144056398196a2030b1152a11d7f6f3162
Tushar Rangaswamy <trangasw@gmu.edu>
2023-03-22 21:03:11 -0400

    GenerateSection method

--- Assets/Scripts/Ground/GroundManager.cs
+++ Assets/Scripts/Ground/GroundManager.cs
@@ -4,18 +4,38 @@ using UnityEngine;
 
 public class GroundManager : MonoBehaviour
 {
-
+    public static GroundManager groundManager;
     public GameObject[] sectionPrefabs;
     public LinkedList<GroundSection> activeSections;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (!groundManager)
+        {
+            groundManager = this;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            if(sectionPrefabs.Length != 0)
+            {
+                CreateNextSection(sectionPrefabs[0]);
+            }
+        }
+    }
+
+    void CreateNextSection(GameObject sectionPrefab) 
+    { 
+    
+        GameObject instance = Instantiate(sectionPrefab);
+        if(activeSections.Count != 0)
+        {
+            instance.transform.position += (Vector3)(activeSections.Last.Value.endPoint - instance.GetComponent<GroundSection>().startPoint);
+        }
+        activeSections.AddLast(new LinkedListNode<GroundSection>(instance.GetComponent<GroundSection>()));
     }
 }
