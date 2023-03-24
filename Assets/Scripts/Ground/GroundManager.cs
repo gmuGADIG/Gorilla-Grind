dc5f45579d7ee0cf05c895acff973a055959abe5
Tushar Rangaswamy <trangasw@gmu.edu>
2023-03-23 21:02:07 -0400

    Auto generate sections and manager editor

--- Assets/Scripts/Ground/GroundManager.cs
+++ Assets/Scripts/Ground/GroundManager.cs
@@ -7,6 +7,7 @@ public class GroundManager : MonoBehaviour
     public static GroundManager groundManager;
     public GameObject[] sectionPrefabs;
     public LinkedList<GroundSection> activeSections;
+    public float spawnOffset;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,28 +16,72 @@ public class GroundManager : MonoBehaviour
             groundManager = this;
         }
         activeSections = new LinkedList<GroundSection>();
+        if(sectionPrefabs.Length == 0)
+        {
+            Debug.LogError("No prefabs loaded in GroundManager");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Z))
+        //if(Input.GetKeyDown(KeyCode.Z))
+        //{
+        //    if(sectionPrefabs.Length != 0)
+        //    {
+        //        CreateNextSection(sectionPrefabs[0]);
+        //    }
+        //}
+        if(sectionPrefabs.Length != 0)
         {
-            if(sectionPrefabs.Length != 0)
+            DespawnCheck();
+            SpawnCheck();
+        }
+
+    }
+    
+    /**
+     * Two assumptions are made here
+     * The first is that the camera and player don't deviate too far from the origin
+     * The second is the player can't traverse backwards
+     */
+
+    void DespawnCheck()
+    {
+        if (activeSections.First != null)
+        {
+            if (activeSections.First.Value.endPoint.x + transform.position.x < -spawnOffset)
             {
-                CreateNextSection(sectionPrefabs[0]);
+                Destroy(activeSections.First.Value.gameObject);
+                activeSections.RemoveFirst();
             }
         }
     }
-
+    void SpawnCheck()
+    {
+        
+        if(activeSections.Last == null || activeSections.Last.Value.endPoint.x + transform.position.x < spawnOffset)
+        {
+            GenerateRandomSection();
+        }
+    }
+    void GenerateRandomSection()
+    {
+        if(sectionPrefabs.Length != 0)
+        {
+            CreateNextSection(sectionPrefabs[Random.Range(0, sectionPrefabs.Length - 1)]);
+        }
+    }
     void CreateNextSection(GameObject sectionPrefab) 
     { 
     
         GameObject instance = Instantiate(sectionPrefab);
-        if(activeSections.Count != 0)
+        if(activeSections.Count != 0)   
         {
             instance.transform.position += (Vector3)(activeSections.Last.Value.endPoint - instance.GetComponent<GroundSection>().startPoint);
+            //Aligning height for now since there is lack of specs for mismatches
+            instance.transform.position += Vector3.up * (activeSections.Last.Value.startPoint.y - instance.GetComponent<GroundSection>().startPoint.y);
         }
-        activeSections.AddLast(new LinkedListNode<GroundSection>(instance.GetComponent<GroundSection>()));
+        activeSections.AddLast(instance.GetComponent<GroundSection>());
     }
 }
