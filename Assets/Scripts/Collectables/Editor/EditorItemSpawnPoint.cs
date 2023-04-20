dbe18bf3064d67be47202a65138469f45326adfc
Tushar Rangaswamy <trangasw@gmu.edu>
2023-04-20 19:36:47 -0400

    Vector math for Item spawning

--- Assets/Scripts/Collectables/Editor/EditorItemSpawnPoint.cs
+++ Assets/Scripts/Collectables/Editor/EditorItemSpawnPoint.cs
@@ -10,13 +10,13 @@ public class EditorItemSpawnPoint : Editor
     {
 
         ItemSpawnPoint t = target as ItemSpawnPoint;
-        Debug.DrawLine(t.transform.position, t.PositionOnLine(t.transform.position));
+        //Debug.DrawLine(t.transform.position, t.PositionOnLine(t.transform.position));
     }
 
     [DrawGizmo(GizmoType.Selected, typeof(ItemSpawnPoint))]
     private static void DrawEdgeBox(ItemSpawnPoint itemSpawn, GizmoType type)
     {
-        GroundEdge edge = itemSpawn.groundEdge;
+        GroundEdge? edge = itemSpawn.groundEdge;
         if (!edge) return;
         Gizmos.color = Color.cyan;
         Gizmos.DrawWireCube((edge.startPoint + edge.endPoint) / 2, (edge.endPoint - edge.startPoint) + Vector2.up * 50);
--- Assets/Scripts/Collectables/ItemSpawnPoint.cs
+++ Assets/Scripts/Collectables/ItemSpawnPoint.cs
@@ -15,7 +15,7 @@ public class ItemSpawnPoint : MonoBehaviour
 
 
     //Gets the associated ground edge
-    public GroundEdge? groundEdge => transform.parent.GetComponent<GroundEdge>();
+    public GroundEdge groundEdge => transform.parent ? transform.parent.GetComponent<GroundEdge>() : null;
 
     public static readonly Vector2 WRONG_HIT = new Vector2(-123, 456); //dummy vec;
 
@@ -38,10 +38,30 @@ public class ItemSpawnPoint : MonoBehaviour
             Instantiate(itemToSpawn, point, Quaternion.identity, transform);
         }
     }
+    //gets the index of the point in the collider that is just before the given point. Returns -1 if out of range.
+    private int PrevPointIndex(Vector2 point)
+    {
+        if (!groundEdge) return -1;
+        for(int i = 0; i < groundEdge.edgeCollider.pointCount; i++)
+        {
+            //If edge point is ahead of given point, return the previous point
+            if(Utils.GetWorldPoint(
+                groundEdge.edgeCollider.points[i], groundEdge.gameObject).x > point.x)
+            {
+                return i - 1;
+            }
+        }
+        return -1;
+    }
 
+    private Vector2 GetSubEdgeVector(int firstInd)
+    {
+        return groundEdge.edgeCollider.points[firstInd] - groundEdge.edgeCollider.points[firstInd + 1];
+    }
     //Returns the projection of the point downward to meet the line specified by the edge collider
     public Vector2 PositionOnLine(Vector2 point)
     {
+        //TODO change this from a raycast to vector math
         RaycastHit2D temp = Physics2D.Raycast(point, Vector2.down);
         if (!temp) return WRONG_HIT;
         if (temp.collider.GetComponent<GroundEdge>() != groundEdge) return WRONG_HIT;
@@ -51,7 +71,9 @@ public class ItemSpawnPoint : MonoBehaviour
  
     public float HeightOffset()
     {
-        return transform.position.y - PositionOnLine(transform.position).y;
+        Vector2 vp = PositionOnLine(transform.position) - (Vector2)transform.position;
+        Vector2 v = GetSubEdgeVector(PrevPointIndex(transform.position));
+        return (vp - (Vector2)Vector3.Project(vp, v)).magnitude;
     }
 
     
@@ -68,11 +90,18 @@ public class ItemSpawnPoint : MonoBehaviour
         throw new System.Exception();
     }
 
+ 
     public Vector2[] GetSpawnPoints()
     {
         //first spawn is at pos
         //subsequent spawns are at an offset
 
         return new Vector2[] { this.transform.position };
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position, 0.5f);
     }
 }
