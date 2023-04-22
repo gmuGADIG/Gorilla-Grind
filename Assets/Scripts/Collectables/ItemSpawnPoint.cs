c7ee5cfe1e7ebca84138e9fad827b8980d430f4e
Tushar Rangaswamy <trangasw@gmu.edu>
2023-04-21 21:47:22 -0400

    The spawn locations is being debugged.

--- Assets/Scripts/Collectables/ItemSpawnPoint.cs
+++ Assets/Scripts/Collectables/ItemSpawnPoint.cs
@@ -11,6 +11,7 @@ public class ItemSpawnPoint : MonoBehaviour
     public float spawnDelta;
 
     [SerializeField]
+    [Range(1, 10)]
     public int spawnAmount;
 
 
@@ -56,7 +57,7 @@ public class ItemSpawnPoint : MonoBehaviour
 
     private Vector2 GetSubEdgeVector(int firstInd)
     {
-        return groundEdge.edgeCollider.points[firstInd] - groundEdge.edgeCollider.points[firstInd + 1];
+        return groundEdge.edgeCollider.points[firstInd+1] - groundEdge.edgeCollider.points[firstInd];
     }
     //Returns the projection of the point downward to meet the line specified by the edge collider
     public Vector2 PositionOnLine(Vector2 point)
@@ -66,14 +67,16 @@ public class ItemSpawnPoint : MonoBehaviour
         if (!temp) return WRONG_HIT;
         if (temp.collider.GetComponent<GroundEdge>() != groundEdge) return WRONG_HIT;
 
+        //Debug.DrawLine(point, temp.point);
         return temp.point;
     }
  
     public float HeightOffset()
     {
         Vector2 vp = PositionOnLine(transform.position) - (Vector2)transform.position;
-        Vector2 v = GetSubEdgeVector(PrevPointIndex(transform.position));
-        return (vp - (Vector2)Vector3.Project(vp, v)).magnitude;
+        return vp.magnitude;
+        //Vector2 v = GetSubEdgeVector(PrevPointIndex(transform.position));
+        //return (vp - (Vector2)Vector3.Project(vp, v)).magnitude;
     }
 
     
@@ -95,13 +98,50 @@ public class ItemSpawnPoint : MonoBehaviour
     {
         //first spawn is at pos
         //subsequent spawns are at an offset
+        Vector2[] targets = new Vector2[spawnAmount];
+        targets[0] = transform.position;
+        float offset = HeightOffset();
+        for(int i = 1; i < spawnAmount; i++)
+        {
+            //find the subedge of the next target
+            int ind = PrevPointIndex(targets[i - 1]);
+            Vector2 subEdge = GetSubEdgeVector(ind);
+            float dist = spawnDelta - (PositionOnLine(targets[i - 1]) - Utils.GetWorldPoint(groundEdge.edgeCollider.points[ind+1], groundEdge.gameObject)).magnitude;
+            Debug.DrawRay(transform.position, -(PositionOnLine(targets[i - 1]) - Utils.GetWorldPoint(groundEdge.edgeCollider.points[ind + 1], groundEdge.gameObject)));
+            while(dist > 0)
+            {
+                ind++;
+                if (ind > groundEdge.edgeCollider.edgeCount)
+                {
+                    //shrink array and break
+                    Vector2[] temp = new Vector2[i];
+                    System.Array.Copy(targets, temp, i);
+                    return temp;
+                }
+                dist -= GetSubEdgeVector(ind).magnitude;
 
-        return new Vector2[] { this.transform.position };
+            }
+            //Debug.DrawRay()
+            targets[i] = (GetSubEdgeVector(ind).magnitude + dist) * GetSubEdgeVector(ind).normalized +
+                Utils.PerpendicularCounterClockwise(GetSubEdgeVector(ind)).normalized * offset +
+                Utils.GetWorldPoint(groundEdge.edgeCollider.points[ind], groundEdge.gameObject);
+            //targets[i] = targets[i] + subEdge.normalized * (subEdge.magnitude + dist);
+
+        }
+
+        return targets;
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawSphere(transform.position, 0.5f);
+        Gizmos.DrawLine(transform.position, GetSubEdgeVector(PrevPointIndex(transform.position))*spawnDelta + (Vector2)transform.position);
+        Gizmos.color = Color.red;
+        foreach (Vector2 item in GetSpawnPoints())
+        {
+            Gizmos.DrawSphere(item, 0.5f);
+            Gizmos.DrawLine(item, PositionOnLine(item));
+        }
     }
 }
