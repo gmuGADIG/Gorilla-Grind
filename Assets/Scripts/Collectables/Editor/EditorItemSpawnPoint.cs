274419bd1c6857dff77299e8798d0d95111aaf3a
Tushar Rangaswamy <trangasw@gmu.edu>
2023-04-27 15:37:33 -0400

    Inspector field for height offset

--- Assets/Scripts/Collectables/Editor/EditorItemSpawnPoint.cs
+++ Assets/Scripts/Collectables/Editor/EditorItemSpawnPoint.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 using UnityEditor;
 
 [CustomEditor(typeof(ItemSpawnPoint))]
+[CanEditMultipleObjects]
 public class EditorItemSpawnPoint : Editor
 {
+
     private void OnSceneGUI()
     {
 
         ItemSpawnPoint t = target as ItemSpawnPoint;
         //Debug.DrawLine(t.transform.position, t.PositionOnLine(t.transform.position));
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+
+        EditorGUI.BeginChangeCheck();
+        float orig = (target as ItemSpawnPoint).HeightOffset();
+        float offset = EditorGUILayout.FloatField("Height Offset", orig);
+
+        if(EditorGUI.EndChangeCheck())
+        {
+            ItemSpawnPoint item = target as ItemSpawnPoint;
+            item.transform.position += (Vector3)Vector2.up * (offset - orig);
+
+        }
+        
     }
 
     [DrawGizmo(GizmoType.Selected, typeof(ItemSpawnPoint))]
