6693f98cc0b3808c4aca3eebcc557fbaa7c3a698
Aegistus <joe.ballanders@gmail.com>
2023-05-03 20:17:27 -0400

    Fix Banana Pickups and Add Banana SFX

--- Assets/Scripts/BananaScript.cs
+++ Assets/Scripts/BananaScript.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class BananaScript : MonoBehaviour
 {
     public BoxCollider2D bananaCollider;
-    public CapsuleCollider2D playerCollider;
+    Collider2D playerCollider;
+
+    int pickupSoundID;
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Banana");
+        pickupSoundID = SoundManager.Instance.GetSoundID("Banana_Pickup");
+        playerCollider = FindObjectOfType<PlayerMovement>().GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
@@ -21,15 +25,16 @@ public class BananaScript : MonoBehaviour
 
     public void Collect()
     {
+        SoundManager.Instance.PlaySoundGlobal(pickupSoundID);
         GameObject.Destroy(gameObject);
     }
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision == playerCollider)
         {
-            Goals_Tracker tracker = collision.gameObject.GetComponent<Goals_Tracker>();
-            tracker.AddBananas(1);
-            Destroy(gameObject);
+            //Goals_Tracker tracker = collision.gameObject.GetComponent<Goals_Tracker>();
+            //tracker.AddBananas(1);
+            Collect();
         }
     }
 }
