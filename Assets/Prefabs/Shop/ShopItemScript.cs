b38a9962939b1e3282679186520495a91fdf3ae9
Aegistus <joe.ballanders@gmail.com>
2023-05-03 21:42:35 -0400

    Add Jingle Implementation Into ShopItemScript.cs

--- Assets/Prefabs/Shop/ShopItemScript.cs
+++ Assets/Prefabs/Shop/ShopItemScript.cs
@@ -11,12 +11,14 @@ public class ShopItemScript : MonoBehaviour
     public string itemName;
     [Tooltip("Number of bananas to deduct from inventory when player purchases item")]
     public int price;
+    public string jingleName = "Jingle_Default";
 
     private bool isPurchased;
     private Button buyButton;
     private TMP_Text buttonText;
 
     int itemBoughtSoundID;
+    int jingleSoundID;
 
     // Start is called before the first frame update
     void Start()
@@ -35,7 +37,8 @@ public class ShopItemScript : MonoBehaviour
             itemName = gameObject.name;
         }
 
-        SoundManager.Instance.GetSoundID("Shop_Purchase_Item");
+        itemBoughtSoundID = SoundManager.Instance.GetSoundID("Shop_Purchase_Item");
+        jingleSoundID = SoundManager.Instance.GetSoundID(jingleName);
     }
 
     // Update is called once per frame
@@ -49,6 +52,7 @@ public class ShopItemScript : MonoBehaviour
             Inventory.addItem(itemName);
             setButtonToPurchased();
             SoundManager.Instance.PlaySoundGlobal(itemBoughtSoundID);
+            SoundManager.Instance.PlaySoundGlobal(jingleSoundID);
         }
     }
 
