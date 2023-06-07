1c53debaff83316cdb9b41f45542bc829284de80
Rainbow Asteroids <rainbow@portal2d.xyz>
2023-06-07 16:43:53 -0400

    Added banana functionality; fix #18

--- Assets/Scripts/BananaScript.cs
+++ Assets/Scripts/BananaScript.cs
@@ -7,9 +7,12 @@ public class BananaScript : MonoBehaviour
     public BoxCollider2D bananaCollider;
     public CapsuleCollider2D playerCollider;
 
+    int BananaSoundID;
+
     // Start is called before the first frame update
     void Start()
     {
+        BananaSoundID = SoundManager.Instance.GetSoundID("Banana_Pickup");
         Debug.Log("Banana");
         playerCollider = GameObject.Find("Player").transform.GetComponent<CapsuleCollider2D>();
     }
@@ -30,6 +33,9 @@ public class BananaScript : MonoBehaviour
         {
             //Goals_Tracker tracker = collision.gameObject.GetComponent<Goals_Tracker>();
             //tracker.AddBananas(1);
+
+            SoundManager.Instance.PlaySoundGlobal(BananaSoundID);
+            Inventory.AddBananas(1);
             Destroy(gameObject);
         }
     }
